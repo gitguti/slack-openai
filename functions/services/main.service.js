276c735a73fc32a5fa4edb6b2dde6e62142a6d98
gitguti <gitmelgtrrz1@gmail.com>
2023-06-10 17:12:57 -0400

    fixed error about generateResume function length

--- functions/services/main.service.js
+++ functions/services/main.service.js
@@ -43,28 +43,121 @@ const findSentiment = async (message) => {
   return sentiment.data.choices[0].text;
 };
 
+const generateResume = async (message) => {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    // eslint-disable-next-line max-len
+    prompt: "Provide a concise summary of the following messages:\n\n" + message,
+    temperature: 0.5,
+    max_tokens: 300,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+
+  });
+  return response.data.choices[0].text;
+};
+
 // Slack Slash Command Handler
 app.command("/openai", async ({command, ack, client}) => {
   // Acknowledge command request
   await ack();
 
-  // Respond with message to user
-  const articleMessage = await client.chat.postMessage({
-    // eslint-disable-next-line max-len
-    text: `:hourglass: *Generating Blog Article* - [Your Input: ${command.text}]`,
-    channel: command.channel_id,
-  });
-    // Generate Article
-  const article = await main(command.text);
-  // Get message timestamp from response
-  const messageTimestamp = articleMessage.ts;
-  // Update Message with Article
-  await client.chat.update({
-    channel: command.channel_id,
-    ts: messageTimestamp,
-    text: `*Article Generated* :white_check_mark: ${article}`,
-  });
+  try {
+    // Obtener la fecha y hora actual
+    const now = new Date();
+    // Restar 10 minutos a la fecha y hora actual
+    const tenMinutesAgo = new Date(now.getTime() - 30 * 60 * 1000);
+
+    // Obtener los mensajes del canal en los últimos 10 minutos
+    const result = await client.conversations.history({
+      channel: command.channel_id,
+      latest: Math.floor(now.getTime() / 1000),
+      oldest: Math.floor(tenMinutesAgo.getTime() / 1000),
+    });
+
+    // Extraer los textos de los mensajes
+    const messages = result.messages.map((message) => message.text);
+
+    // Concatenar todos los mensajes en un solo texto
+    const allMessages = messages.join("\n");
+
+    // Generar el resumen del texto completo
+    const summary = await generateResume(allMessages);
+
+    // Enviar el resumen al canal
+    await client.chat.postMessage({
+      text: `Resumen de los mensajes de los últimos 10 minutos:\n\n${summary}`,
+      channel: command.channel_id,
+    });
+  } catch (error) {
+    console.error("Error al obtener los mensajes del canal:", error);
+  }
 });
+
+
+// // Slack Slash Command Handler
+// app.command("/openai", async ({command, ack, client}) => {
+//   // Acknowledge command request
+//   await ack();
+
+//   // Respond with message to user
+//   const articleMessage = await client.chat.postMessage({
+//     // eslint-disable-next-line max-len
+//     text: `:hourglass: *Generating Blog Article* - [
+// Your Input: ${command.text}]`,
+//     channel: command.channel_id,
+//   });
+//     // Generate Article
+//   const article = await main(command.text);
+//   // Get message timestamp from response
+//   const messageTimestamp = articleMessage.ts;
+//   // Update Message with Article
+//   await client.chat.update({
+//     channel: command.channel_id,
+//     ts: messageTimestamp,
+//     text: `*Resume Generated* :white_check_mark:
+//     resumen: ${article.response.resume},
+//     keywords: ${article.response.keywords},
+//     sentimientos: ${article.response.sentiment}`,
+//   });
+// });
+
+// // Slack Slash Command Handler
+// app.command("/openai", async ({command, ack, client}) => {
+//   // Acknowledge command request
+//   await ack();
+
+//   try {
+//     // Obtener la fecha de hace 10 minutos
+//     const tenMinutesAgo = Math.floor(Date.now() / 1000) - (10 * 60);
+//     // Obtener la marca de tiempo de hace 10 minutos
+
+//     // Obtener los mensajes del canal en las últimas 6 horas
+//     const result = await client.conversations.history({
+//       channel: command.channel_id,
+//       latest: tenMinutesAgo.toString(),
+//     });
+
+//     // Extraer los textos de los mensajes
+//     const messages = result.messages.map((message) => message.text);
+
+//     // Concatenar todos los mensajes en un solo texto
+//     const allMessages = messages.join("\n");
+
+//     // Generar un resumen del texto completo
+//     const summary = await generateResume(allMessages);
+
+//     // Enviar el resumen al canal
+//     await client.chat.postMessage({
+//     text: `Resumen de los mensajes de las últimos 10 minutos:\n\n${summary}`,
+//       channel: command.channel_id,
+//     });
+//   } catch (error) {
+//     console.error("Error al obtener los mensajes del canal:", error);
+//   }
+// });
+
 
 (async () => {
   // Start your app
@@ -78,7 +171,8 @@ const main = async (message) => {
   try {
     const keywords = await findKeyword(message);
     const sentiment = await findSentiment(message);
-    return {response: {keywords, sentiment}, status: 200};
+    const resume = await generateResume(message);
+    return {response: {keywords, sentiment, resume}, status: 200};
   } catch (error) {
     if (error) {
       return {response: {result: error}, status: 500};
